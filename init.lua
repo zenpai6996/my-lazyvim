56730f8e4720269cd86f3e920ea8accebed79da8
zenpai6996 <souhardaroybarman05@gmail.com>
2025-05-01 16:17:57 +0530

    Initial commit: My Neovim + LazyVim setup

---
+++ init.lua
@@ -0,0 +1,4 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+package.path = package.path .. ";~/.local/share/lua/5.1/?.lua"
---
+++ lua/config/init.lua
@@ -0,0 +1,1 @@
+package.path = package.path .. ";~/.local/share/lua/5.1/?.lua"
